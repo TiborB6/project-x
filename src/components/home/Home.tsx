869ee1782698d927b55f4fb3a82f3c5f230d6e5a
TiborB <btibor@gmx.at>
2023-07-17 22:57:18 +0200

    Dev 3
    + added img carussel
    + animations

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { imagesBlack, imagesWhite } from '../img/IMG_IMPORT_Module'
 import Nav from '../navbar/Nav'
 import { useSelector } from 'react-redux'
@@ -7,12 +7,39 @@ import './home.scss'
 export default function Home (): JSX.Element {
   const theme = useSelector((state: { theme: { theme: string } }) => state.theme.theme)
 
-  const imagesLight = imagesWhite as string[]
-  const imagesDark = imagesBlack as string[]
+  const [i, setI] = useState(0)
 
-  const [i] = useState(0)
+  if (imagesBlack.length !== imagesWhite.length) {
+    console.error('images Black not maching images white in home')
+  }
+  const length = imagesBlack.length
 
-  console.log(imagesLight)
+  const handleRightClick = (): void => {
+    if (i > length - 2) {
+      setI(0)
+    } else {
+      setI(i + 1)
+    }
+  }
+
+  const handleLeftClick = (): void => {
+    if (i < 1) {
+      setI(length - 1)
+    } else {
+      setI(i - 1)
+    }
+  }
+
+  useEffect(() => {
+    const prevActive = document.querySelector('.activePic')
+    prevActive?.classList.remove('activePic')
+    const newActive = document.querySelector(`#_${i}`)
+    newActive?.classList.add('activePic')
+
+    setTimeout(() => {
+      handleRightClick()
+    }, 5000)
+  }, [i])
 
   return (
     <>
@@ -21,15 +48,46 @@ export default function Home (): JSX.Element {
         <div className="img-carussel">
           {theme === 'light'
             ? (
-            <img src={imagesLight[i]} alt="Vape White" />
-              )
+              <TransitionImage key={imagesWhite[i]} src={imagesWhite[i]} alt="Vape White" />)
             : (
-            <img src={imagesDark[i]} alt="Vape Black" />
-              )}
-          <button></button>
-          <button></button>
+              <TransitionImage key={imagesBlack[i]} src={imagesBlack[i]} alt="Vape Black" />)}
+          <button className="left" onClick={handleLeftClick}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+              <path d="M530-481 332-679l43-43 241 241-241 241-43-43 198-198Z" strokeWidth="5"/>
+            </svg>
+
+          </button>
+          <button className="right" onClick={handleRightClick}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M561-240 320-481l241-241 43 43-198 198 198 198-43 43Z"/></svg>
+          </button>
+          <div className="carussel-display">
+            {imagesBlack.map((item, index) => (
+              <div key={index} id={'_' + index.toString()}></div>
+            ))}
+          </div>
         </div>
       </div>
     </>
+  )
+}
+
+interface TransitionImageProps {
+  src: string
+  alt: string
+}
+
+const TransitionImage: React.FC<TransitionImageProps> = ({ src, alt }) => {
+  const [showImage, setShowImage] = useState(false)
+
+  useEffect(() => {
+    setShowImage(true)
+  }, [src])
+
+  return (
+    <img
+      className={`image-transition ${showImage ? 'show' : ''}`}
+      src={src}
+      alt={alt}
+    />
   )
 }
