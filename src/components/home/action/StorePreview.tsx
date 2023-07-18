28eebbc35da1f7237267ebd6a39d2c7a0f29d084
TiborB <btibor@gmx.at>
2023-07-18 21:12:07 +0200

    dev 7
    + store preview
    + styling

--- src/components/home/action/StorePreview.tsx
+++ src/components/home/action/StorePreview.tsx
@@ -1,29 +1,39 @@
-import React, { useState } from 'react'
+import React from 'react'
 import PreviewDisplay from './preview-display/PreviewDisplay'
+import { useNavigate } from 'react-router-dom'
 
 export default function StorePreview (): JSX.Element {
-  const [productDisplay, setProductDisplay] = useState('E-Zigaretten')
-
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>): void => {
-    const currentDispayed = document.querySelector('.displayed')
-    currentDispayed?.classList.remove('displayed')
+    const currentDisplayed = document.querySelector('.displayed')
+    currentDisplayed?.classList.remove('displayed')
 
-    setProductDisplay(event.currentTarget.id)
-
-    const newDispayed = document.querySelector(`#${event.currentTarget.id}`)
-    newDispayed?.classList.add('displayed')
+    const newDisplayed = document.querySelector(`#${event.currentTarget.id}`)
+    newDisplayed?.classList.add('displayed')
   }
+
+  const navigate = useNavigate()
 
   return (
     <div className="preview">
-      <h1>Bestseller</h1>
+      <div className="header">
+        <h1>Bestseller</h1>
+        <button onClick={() => { navigate('/store') }}>{'More Products >>>'}</button>
+      </div>
       <div className="preview-selection">
-        <button onClick={(event) => { handleClick(event) }} className="displayed" id="e-vape">E-Zigaretten</button>
-        <button onClick={(event) => { handleClick(event) }} id="g-vape">Vaporizer</button>
-        <button onClick={(event) => { handleClick(event) }} id="l-vape">Liquids</button>
+        <button onClick={handleClick} className="displayed" id="e-vape">
+          E-Zigaretten
+        </button>
+        <button onClick={handleClick} id="g-vape">
+          Vaporizer
+        </button>
+        <button onClick={handleClick} id="l-vape">
+          Liquids
+        </button>
       </div>
       <div className="store-preview">
-        <PreviewDisplay name={productDisplay}/>
+        {[...Array(4)].map((_, index) => (
+          <PreviewDisplay key={index} id={`preview-${index}`} />
+        ))}
       </div>
     </div>
   )
