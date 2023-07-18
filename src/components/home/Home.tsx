e5ba8d71734a1511042ac08675db1eed76cc4ac5
TiborB <btibor@gmx.at>
2023-07-18 11:28:06 +0200

    Dev 4
    + section SectionOne
    +styling

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Nav from '../navbar/Nav'
 import './home.scss'
 import ImgCarussel from './attention-interest/ImgCarussel'
+import SectionOne from './desire/SectionOne'
 
 export default function Home (): JSX.Element {
   return (
     <>
       <Nav />
       <ImgCarussel />
+      <SectionOne />
     </>
   )
 }
