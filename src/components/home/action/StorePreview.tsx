8ed8315dd680188ff9a11caa16d00ef30c933394
TiborB <btibor@gmx.at>
2023-07-19 12:42:37 +0200

    Commented Code

--- src/components/home/action/StorePreview.tsx
+++ src/components/home/action/StorePreview.tsx
@@ -3,23 +3,29 @@ import PreviewDisplay from './preview-display/PreviewDisplay'
 import { useNavigate } from 'react-router-dom'
 
 export default function StorePreview (): JSX.Element {
+  // Function to handle the click event on the selection buttons
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>): void => {
+    // Remove the 'displayed' class from the currently displayed button
     const currentDisplayed = document.querySelector('.displayed')
     currentDisplayed?.classList.remove('displayed')
 
+    // Add the 'displayed' class to the clicked button
     const newDisplayed = document.querySelector(`#${event.currentTarget.id}`)
     newDisplayed?.classList.add('displayed')
   }
 
+  // Use the useNavigate hook from react-router-dom to get the navigate function
   const navigate = useNavigate()
 
   return (
     <div className="preview">
       <div className="header">
+        {/* Header with the title and a button to navigate to the store page */}
         <h1>Bestseller</h1>
         <button onClick={() => { navigate('/store') }}>{'More Products >>>'}</button>
       </div>
       <div className="preview-selection">
+        {/* Selection buttons for different product types */}
         <button onClick={handleClick} className="displayed" id="e-vape">
           E-Zigaretten
         </button>
@@ -31,7 +37,9 @@ export default function StorePreview (): JSX.Element {
         </button>
       </div>
       <div className="store-preview">
+        {/* Use Array.map to render multiple instances of the PreviewDisplay component */}
         {[...Array(4)].map((_, index) => (
+          // Pass a unique key and an id prop to each PreviewDisplay component
           <PreviewDisplay key={index} id={`preview-${index}`} />
         ))}
       </div>
--- src/store.ts
+++ src/store.ts
@@ -1,20 +1,27 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit'
 
+// Define the initial state for the theme
 const initialState = {
   theme: 'dark'
 }
 
+// Define the action type for toggling the theme
 const TOGGLE_THEME = 'TOGGLE_THEME'
+
+// Define the action interface for the ToggleThemeAction
 interface ToggleThemeAction {
   type: typeof TOGGLE_THEME
 }
 
+// Define the action creator function for toggling the theme
 export const toggleTheme = (): ToggleThemeAction => {
   return { type: TOGGLE_THEME }
 }
 
+// Define the themeReducer to handle the theme state changes
 const themeReducer = (state = initialState, action: ToggleThemeAction): typeof initialState => {
   switch (action.type) {
+    // When TOGGLE_THEME action is dispatched, toggle the theme between 'light' and 'dark'
     case TOGGLE_THEME:
       return { ...state, theme: state.theme === 'light' ? 'dark' : 'light' }
     default:
@@ -22,15 +29,19 @@ const themeReducer = (state = initialState, action: ToggleThemeAction): typeof i
   }
 }
 
+// Combine all reducers using combineReducers
 const rootReducer = combineReducers({
   theme: themeReducer
 })
 
+// Configure the Redux store with the rootReducer
 const store = configureStore({
   reducer: rootReducer
 })
 
+// Define types for RootState and AppDispatch based on the store
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
 
+// Export the store
 export default store
