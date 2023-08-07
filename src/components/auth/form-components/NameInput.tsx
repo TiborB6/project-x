f8aa87417a63049a66d61ee0d082a40288d088cb
TiborB <btibor@gmx.at>
2023-08-07 23:53:01 +0200

    Migration 7
    + added form component

---
+++ src/components/auth/form-components/NameInput.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import './form-element.scss'
+interface Name {
+  type: 'first-name' | 'second-name'
+  changeFunction: (value: string) => void
+}
+
+type ValidationState = 'valid' | 'invalid' | 'empty'
+
+export default function NameInput ({ type, changeFunction }: Name): JSX.Element {
+  const [error, setError] = useState('')
+  const [validation, setValidation] = useState<ValidationState>('empty')
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    if (event.target.value.length < 1) {
+      setValidation('empty')
+      setError('')
+    } else if (event.target.value.length > 100) {
+      setValidation('invalid')
+      setError(' - max. length 100 character')
+    } else {
+      setValidation('valid')
+      setError('')
+      const value = event.target.value
+      changeFunction(value)
+    }
+  }
+
+  return (
+    <div className="form-wrapper">
+      <input type="text" name={type} id={type} onChange={handleChange} className={validation} autoComplete="off"/>
+      <div></div>
+      <label htmlFor="first-name">
+        {type === 'first-name' ? 'First Name' : 'Second Name'}
+        <span className='error'>{error}</span>
+      </label>
+    </div>
+  )
+}
---
+++ src/components/auth/form-components/PhoneInput.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import './form-element.scss'
+interface Phone {
+  changeFunction: (value: string) => void
+}
+
+type ValidationState = 'valid' | 'invalid' | 'empty'
+
+export default function PhoneInput ({ changeFunction }: Phone): JSX.Element {
+  const [error, setError] = useState('')
+  const [validation, setValidation] = useState<ValidationState>('empty')
+
+  const internationalPhoneNumberPattern = /^\+(?:[0-9] ?){6,14}[0-9]$/
+  const handlePhoneChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    const value = event.target.value.trim()
+
+    if (value === '') {
+      setError('')
+      setValidation('empty')
+    } else if (!value.startsWith('+')) {
+      setError(' - International country code missing (+)')
+      setValidation('invalid')
+    } else if (!internationalPhoneNumberPattern.test(value)) {
+      setError(' - Enter a correct phone number')
+      setValidation('invalid')
+    } else if (value.length > 150) {
+      setError(' - Max. length 150 characters')
+      setValidation('invalid')
+    } else {
+      setError('')
+      setValidation('valid')
+      changeFunction(value)
+    }
+  }
+
+  return (
+    <div className="form-wrapper">
+      <input type="tel" name="tel" id="tel" onChange={handlePhoneChange} className={validation} autoComplete="off"/>
+      <div></div>
+      <label htmlFor="tel">Phone
+        <span className='error'>{error}</span>
+      </label>
+    </div>
+  )
+}
