c5cce088a42961a2ffe8129560466c5aa3d93ac6
TiborB <btibor@gmx.at>
2023-08-11 00:55:02 +0200

    added correct env

--- src/mongoDB/db.ts
+++ src/mongoDB/db.ts
@@ -1,9 +1,8 @@
 import mongoose from 'mongoose'
-import { MDB_URI } from '../../config'
 
 export default async function connectDB (): Promise<void> {
   try {
-    await mongoose.connect(MDB_URI)
+    await mongoose.connect(process.env.MDB_URI)
     console.log('connected')
   } catch (err) {
     console.error(err)
