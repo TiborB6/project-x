0a813ad42d26262d1fb96c9c9bfc3827fbc4ec49
TiborB <btibor@gmx.at>
2023-08-14 17:29:28 +0200

    added b2b pages

---
+++ src/pages/b2b.tsx
@@ -0,0 +1,16 @@
+import InfoContact from '@/components/contact/InfoContact'
+import Footer from '@/components/footer/Footer'
+import Nav from '@/components/navbar/Nav'
+import React from 'react'
+
+export default function B2B (): JSX.Element {
+  return (
+    <>
+      <Nav />
+        <div className="b2b">
+
+        </div>
+      <Footer />
+    </>
+  )
+}
